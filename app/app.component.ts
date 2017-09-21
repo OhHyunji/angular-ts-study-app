413fc94c59069ccd8e20aae20244551229019e48
awesome <hyunji9212@gmail.com>
2017-09-21 22:13:18 +0900

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `<h1>hello {{ name }}</h1>`
+})
+export class AppComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'azalea';
+    }
+}
