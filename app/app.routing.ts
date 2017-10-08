bc094d492396bd2026a07cd670c2c7277f8e6a63
awesome <hyunji9212@gmail.com>
2017-10-08 19:23:06 +0900

    [ch3.1.3] navigate() 함수 실습

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,7 +2,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home.component';
 import {ProductComponent} from './components/product.component';
 
-export const routes: Routes = [
+export const routes : Routes = [
     {path: '', component: HomeComponent},
     {path: 'product', component: ProductComponent}
 ];
--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,12 +1,19 @@
 import {Component} from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
     selector: 'app',
     template: `
       <a [routerLink]="['/']">Home</a>
       <a [routerLink]="['/product']">Product</a>
+      <input type="button" value="Product" (click)="navigateToProduct()"/>
       <router-outlet></router-outlet>
     `
 })
 export class AppComponent {
+    constructor(private router : Router) {
+    }
+
+    navigateToProduct() {
+        this.router.navigate(['/product']);
+    }
 }
