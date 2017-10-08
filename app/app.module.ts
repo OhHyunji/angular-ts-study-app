5ec383491f375f4ab7b12c659499a72d16d5fda8
awesome <hyunji9212@gmail.com>
2017-10-08 19:31:00 +0900

    [ch3.1.3] error 404 처리 실습

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app.component';
 import {HomeComponent} from './components/home.component';
 import {ProductComponent} from './components/product.component';
+import {Error404Component} from './components/error404.component';
 import {LocationStrategy, HashLocationStrategy}from '@angular/common';
 import {routing} from './app.routing';
 
@@ -11,7 +12,8 @@ import {routing} from './app.routing';
     declarations: [
         AppComponent,
         HomeComponent,
-        ProductComponent
+        ProductComponent,
+        Error404Component
     ],
     providers: [{provide: LocationStrategy, useClass:HashLocationStrategy}],
     bootstrap: [AppComponent]
---
+++ app/components/error404.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'error404',
+    template: '<h1>Error 404</h1>'
+})
+export class Error404Component {
+}
